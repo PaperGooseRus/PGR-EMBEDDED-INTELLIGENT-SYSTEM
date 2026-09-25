c34eddb9905b8dd0f1bb25e0cab7eb0b9a6dd2ae
Gusev Danil <d.gusev@air-burg.ru>
2026-09-25 14:02:16 +0500

    Определение классов

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+class Base_data {
+    private:
+        // pass
+
+    public:
+        Base_data() {
+            std::cout << "Base data is init" << std::endl;
+        };
+
+        ~Base_data() {
+            std::cout << "Base data is uninit" << std::endl;
+        };
+};
+
+class Markov {
+    private:
+        // pass
+    
+    public:
+        Markov() {
+            std::cout << "Markov is init" << std::endl;
+        };
+
+        ~Markov() {
+            std::cout << "Markov is uninit" << std::endl;
+        };
+};
+
 class Net {
     private:
         // pass
@@ -14,10 +42,27 @@ class Net {
         };
 };
 
-int main(int argc, char *argv[]) {
-    std::cout << "StartUp PaperGooseRus Net" << std::endl;
+class Simvolic_regression {
+    private:
+        // pass
 
+    public:
+        Simvolic_regression() {
+            std::cout << "Simvolic regression is init" << std::endl;
+        };
+
+        ~Simvolic_regression() {
+            std::cout << "Simvolic regression is uninit" << std::endl;
+        };
+};
+
+int main(int argc, char *argv[]) {
+    std::cout << "StartUp PaperGooseRus EMBEDDED NET" << std::endl;
+
+    Base_data base_data;
+    Markov markov;
     Net net;
+    Simvolic_regression simvolic_regression;
 
     return 0;
 }
